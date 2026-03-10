894c805b165ff0f100afd5e55559ff533632e3aa
Lolita Korolova <lolita_korolova@epam.com>
2026-03-10 14:24:56 +0200

    feat: initial commit

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav>
+        <div>
+            <div>
+                <Link href='/'>HomeLib</Link>
+            </div>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
